897e10c915ddc4f166a3093b523d20d63d1797d6
JoshGenereux <josh.genereux13@gmail.com>
2022-01-20 16:39:51 -0700

    finished evenoddarray

--- src/components/Topics/EvenAndOdd.js
+++ src/components/Topics/EvenAndOdd.js
@@ -1,27 +1,54 @@
-import React, {useState} from 'react';
+import React from 'react';
 
 class EvenAndOdd extends React.Component{
     constructor(props) {
         super(props);
-        const evenArray = [];
-        const oddArray = [];
-        const userInput = "";
+        this.state = {
+            evenArray: [],
+            oddArray: [],
+            userInput: ''
+        }
     }
 
-    handleChange(event){
-        this.setState({userInput: event.userInput.value})
+    handleChange(e){
+        this.setState({userInput: e.target.value})
+    }
+
+    handleClick(){
+        let input = this.state.userInput;
+        let array = [...input]
+        const evens = array.filter((num)=>{
+            if(!isNaN(Number(num))) {
+                if (num % 2 === 0) {
+                    return num
+                }
+            }
+        })
+        this.setState({evenArray: [...evens]})
+
+        const odds = array.filter((num)=>{
+            if(!isNaN(Number(num))) {
+                if (num % 2 !== 0) {
+                    return num
+                }
+            }
+        })
+        this.setState({oddArray: [...odds]})
+        console.log(this.state.oddArray)
     }
 
     render() {
         return (
             <div className='puzzleBox evenAndOddPB'>
                 <h4>Evens and Odds</h4>
-                <input onChange={this.handleChange} className='inputLine'/>
+                <input onChange={(e)=>this.handleChange(e)} className='inputLine'/>
                 <button
-                    onClick={}
+                    onClick={()=>(
+                        this.handleClick()
+                    )}
                     className='confirmationButton'>Confirm</button>
-                <span className='resultsBox'></span>
-                <span className='resultsBox'></span>
+                <span className='resultsBox'>{this.state.evenArray}</span>
+                <span className='resultsBox'>{this.state.oddArray}</span>
             </div>
         )
     }
