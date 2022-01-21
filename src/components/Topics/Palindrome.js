9dade5309557c63c2d2d59526f7d8e27a99deddf
JoshGenereux <josh.genereux13@gmail.com>
2022-01-21 11:44:09 -0700

    finished Palindrome

--- src/components/Topics/Palindrome.js
+++ src/components/Topics/Palindrome.js
@@ -1,9 +1,49 @@
 import React from 'react';
 
-function Palindrome(){
-    return (
-        <p>Palindrome</p>
-    )
+class Palindrome extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            userInput: '',
+            palindrome: ''
+        }
+    }
+
+    handleChange(e){
+        this.setState({userInput: e.target.value})
+    }
+
+    handleClick(){
+        let input = this.state.userInput;
+        console.log(input)
+        let string = '';
+        for(let i = input.length-1; i >= 0; i--){
+            string += input[i]
+        }
+        if(string === input){
+            this.setState({palindrome: `${string} is a Palindrome!`})
+        } else {
+            this.setState({palindrome: `${string} is not a Palindrome...`})
+        }
+        // this.setState({palindrome: string})
+    }
+
+    render() {
+        return (
+            <div className='puzzleBox filterStringPB'>
+                <h4>Palindrome</h4>
+                <input
+                    className='inputLine'
+                    onChange={(e)=>this.handleChange(e)}
+                />
+                <button
+                    className='confirmationButton'
+                    onClick={()=>this.handleClick()}
+                >Confirm</button>
+                <span className='resultsBox'>{this.state.palindrome}</span>
+            </div>
+        )
+    }
 }
 
 export default Palindrome;
