44976f698fdab596f422bb9b08f5015aaa8457c6
JoshGenereux <josh.genereux13@gmail.com>
2022-01-21 10:17:50 -0700

    finished filteredString

--- src/components/Topics/FilterString.js
+++ src/components/Topics/FilterString.js
@@ -1,9 +1,59 @@
 import React from 'react';
 
-function FilterString(){
-    return (
-        <p>FilterString</p>
-    )
+class FilterString extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            unFilteredArray: ['hello', 'goodbye', 'adios', 'bonjour','aloha','farewell'],
+            userInput: '',
+            filteredArray: []
+        }
+    }
+
+    handleChange(e){
+        this.setState({userInput: e.target.value})
+    }
+
+    handleClick(){
+        let input = this.state.userInput;
+        let string = ''
+        let filtered = this.state.unFilteredArray.filter((element) =>{
+            if(element.includes(input)){
+                string += `${element},\n`
+                console.log(element)
+            }
+        })
+        this.setState({filteredArray: string})
+    }
+
+    unfiltered(){
+        let array = this.state.unFilteredArray;
+        let string = '';
+        for(let i = 0; i < array.length; i++){
+            let obj = JSON.stringify(array[i])
+            string += `${obj},\n`
+        }
+        return string;
+    }
+
+    render() {
+        return (
+            <div className='puzzleBox filterStringPB'>
+                <h4>Filter String</h4>
+                <span className='puzzleText'>{this.unfiltered()}</span>
+                <input
+                    className='inputLine'
+                    onChange={(e)=>this.handleChange(e)}
+                />
+                <button
+                    className='confirmationButton'
+                    onClick={()=>this.handleClick()}
+                >Confirm</button>
+                <span className='resultsBox filterStringRB'>{this.state.filteredArray}</span>
+            </div>
+        );
+    }
+
 }
 
 export default FilterString;
